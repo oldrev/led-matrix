d6770ddf0058f6e0950165c8460b7e00f5f7fad7
李维 <oldrev@gmail.com>
2019-11-04 12:09:18 +0800

    文档调整，增加色盘颜色

--- firmware/Src/settings.c
+++ firmware/Src/settings.c
@@ -11,9 +11,12 @@ const AppSettings APP_SETTINGS_DEFAULT = { .DisplaySettings = { .LedCount = 500,
                                                .XSkip = 1, // 行内跳过的
                                                .YSkip = 2, // 列之间跳过的
                                                .Palette = {
-                                                   { 0x00, 0x00, 0x00 },
-                                                   { 0x3F, 0x3F, 0x3F },
-                                                   { 0x6F, 0x00, 0x00 },
-                                                   { 0x00, 0x6F, 0x00 },
-                                                   { 0x00, 0x00, 0x6F },
+                                                   { 0x00, 0x00, 0x00 }, // 纯黑
+                                                   { 0x80, 0x80, 0x80 }, // 白/灰
+                                                   { 0x80, 0x00, 0x00 }, // 红
+                                                   { 0x00, 0x80, 0x00 }, // 绿
+                                                   { 0x00, 0x00, 0x80 }, // 蓝
+                                                   { 0x80, 0x80, 0x00 }, // 黄
+                                                   { 0x00, 0x80, 0x80 }, // 青
+                                                   { 0x80, 0x00, 0x80 }, // 粉红
                                                } } };
